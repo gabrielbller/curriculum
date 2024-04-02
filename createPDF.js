79630654c1dc94eb7a914dd69c85532db164dda7
Gabriel Bodenmuller <gabrielbller@icloud.com>
2024-04-02 16:04:49 -0300

    Update

--- createPDF.js
+++ createPDF.js
@@ -3,23 +3,22 @@ function openPrintDialog() {
 }
 
 function changeLanguage() {
-    var elementsToTranslate = document.querySelectorAll('.translate');
+  var elementsToTranslate = document.querySelectorAll(".translate");
 
-    elementsToTranslate.forEach(function(element) {
-        if (element.getAttribute('data-pt')) {
-            if (element.innerText.trim() === element.getAttribute('data-pt').trim()) {
-                element.innerText = element.getAttribute('data-en');
-            } else {
-                element.innerText = element.getAttribute('data-pt');
-            }
-        }
-    });
-
-    var button = document.querySelector('.button-language');
-    if (button.innerText === 'ENGLISH') {
-        button.innerText = 'PORTUGUÊS';
-    } else {
-        button.innerText = 'ENGLISH';
+  elementsToTranslate.forEach(function (element) {
+    if (element.getAttribute("data-pt")) {
+      if (element.innerText.trim() === element.getAttribute("data-pt").trim()) {
+        element.innerText = element.getAttribute("data-en");
+      } else {
+        element.innerText = element.getAttribute("data-pt");
+      }
     }
-}
+  });
 
+  var button = document.querySelector(".button-language");
+  if (button.innerText === "ENGLISH") {
+    button.innerText = "PORTUGUÊS";
+  } else {
+    button.innerText = "ENGLISH";
+  }
+}
